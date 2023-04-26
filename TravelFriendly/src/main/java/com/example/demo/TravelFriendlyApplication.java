c1ba1e902633e669c569b4dd2160e1d5f4b01631
reinachikita <violetatrujilloalmeida@gmail.com>
2023-04-26 13:56:05 +0200

    lastActCache

--- TravelFriendly/src/main/java/com/example/demo/TravelFriendlyApplication.java
+++ TravelFriendly/src/main/java/com/example/demo/TravelFriendlyApplication.java
@@ -13,10 +13,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.config.JoinConfig;
 
 @SpringBootApplication(scanBasePackages={
 		"es.codeurjc.web.service", "com.example"})
