5c8b31f51be20a0c5d127195052771a401b9d873
Pedrobt1003 <pedrobt1003@gmail.com>
2023-05-02 18:10:20 +0200

    update database

--- TravelFriendly/src/main/java/com/example/demo/service/DatabaseInitalizer.java
+++ TravelFriendly/src/main/java/com/example/demo/service/DatabaseInitalizer.java
@@ -30,30 +30,7 @@ public class DatabaseInitalizer {
 	@PostConstruct
 	public void init() throws IOException, URISyntaxException {
 		
-		rUser.save(new User("user", passwordEncoder.encode("pass"), "user@gmail.com","USER"));
-		rUser.save(new User("admin", passwordEncoder.encode("adminpass"), "admin@gmail.com", "USER", "ADMIN"));
-		rUser.save(new User("domingo", passwordEncoder.encode("pass"), "domingo@gmail.com","USER"));
 		
-		
-		User u1 = new User("Matias", passwordEncoder.encode("Sarajevo"), "sarajevo@gmail.com","USER");
-		User u2 = new User("Marcos", passwordEncoder.encode("Zagreb"), "zagreb@gmail.com","USER");
-		User u3 = new User("Bernardo", passwordEncoder.encode("Transilvania"), "transilvania@gmail.com","USER");
-		
-		rUser.save(u1);
-		rUser.save(u2);
-		rUser.save(u3);
-		
-		Trip t1 = new Trip("Badajoz", "Madrid","2023-02-17",2,1,"vamonos");
-		Trip t2 = new Trip("Montijo", "Caceres","2023-03-19",3,1,"paseito");
-		Trip t3 = new Trip("Valencia", "Bilbao","2023-03-22",1,1,"cruzada");
-		
-		t1.SetConductor(u1);
-		t2.SetConductor(u2);
-		t3.SetConductor(u3);
-		
-		rTrip.save(t1);
-		rTrip.save(t2);
-		rTrip.save(t3);
 	
 		
 	}
